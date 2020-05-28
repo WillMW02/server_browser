bc53868055dcc6316ad31c34ac37ce4960f4c547
b_boy_ww <williammw.wmw@gmail.com>
2020-05-28 21:48:31 +0100

    config modification to change to URL based images

--- server_browser/lua/server_browser/browser_config.lua
+++ server_browser/lua/server_browser/browser_config.lua
@@ -4,9 +4,45 @@ serverBrowser.command = "!test"
 
 serverBrowser.servers = {
     {
-        name    = "Gmod Server",
-        ip      = "127.0.0.1",
-        port    = "1234",
-        image   = ""
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
+    },
+    {
+        name        = "Gmod Server",
+        ip          = "127.0.0.1",
+        port        = "1234",
+        imageUrl    = "https://i.imgur.com/zSWe3hv.jpg"
     }
 }
