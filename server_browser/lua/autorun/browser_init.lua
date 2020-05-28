95fbb041689dda0472f037bcd7511b5c131cd518
b_boy_ww <williammw.wmw@gmail.com>
2020-05-28 16:52:25 +0100

    added file includes

---
+++ server_browser/lua/autorun/browser_init.lua
@@ -0,0 +1,14 @@
+
+if SERVER then AddCSLuaFile("server_browser/browser_config.lua") end
+include("server_browser/browser_config.lua")
+
+if SERVER then
+    AddCSLuaFile("server_browser/client/cl_networking.lua")
+    AddCSLuaFile("server_browser/vgui/cl_main.lua")
+
+    include("server_browser/server/sv_hooks.lua")
+    include("server_browser/server/sv_networking.lua")
+else
+    include("server_browser/client/cl_networking.lua")
+    include("server_browser/vgui/cl_main.lua")
+end
