baf666c2c5132c834bd702941a94e33b5efd6cf6
b_boy_ww <williammw.wmw@gmail.com>
2020-05-28 22:08:05 +0100

    fixed reference to clientside utils

--- server_browser/lua/autorun/browser_init.lua
+++ server_browser/lua/autorun/browser_init.lua
@@ -6,12 +6,14 @@ include("server_browser/browser_config.lua")
 if SERVER then
     AddCSLuaFile("server_browser/client/cl_networking.lua")
     AddCSLuaFile("server_browser/vgui/cl_main.lua")
+    AddCSLuaFile("server_browser/client/cl_utils.lua")
 
     include("server_browser/server/sv_hooks.lua")
     include("server_browser/server/sv_networking.lua")
 else
     include("server_browser/client/cl_networking.lua")
     include("server_browser/vgui/cl_main.lua")
+    include("server_browser/client/cl_utils.lua")
 
     for k,v in pairs(serverBrowser.servers) do
         loadUrl(v.imageUrl)
