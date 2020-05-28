948b79b4763db1054388449b4edac60fad0e46f8
b_boy_ww <williammw.wmw@gmail.com>
2020-05-28 16:47:14 +0100

    added config

---
+++ server_browser/lua/server_browser/browser_config.lua
@@ -0,0 +1,12 @@
+serverBrowser = serverBrowser or {}
+
+serverBrowser.command = "!test"
+
+serverBrowser.servers = {
+    {
+        name    = "Gmod Server",
+        ip      = "127.0.0.1",
+        port    = "1234",
+        image   = ""
+    }
+}
